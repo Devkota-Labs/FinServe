2eb993e7ed04ddc7fb431550c24db126e99482a9
devkotadhurba <devkotadhurba@gmail.com>
2025-12-14 18:10:13 +0530

    Update next.config.js

--- frontend/webapp/next.config.js
+++ frontend/webapp/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import("next").NextConfig} */ 
 const nextConfig = 
 { 
-  output: 'export', // enables static export 
+  output: 'export', // enables static export  
 }; 
 
 module.exports = nextConfig;
